8cc0d6564c1497064b807b8a5877d38fc70692fe
Patricio Requena <rprequenac@gmail.com>
2022-03-21 16:36:05 -0500

    Webapp finished with predictions

--- webapp/api/main.py
+++ webapp/api/main.py
@@ -1,14 +1,21 @@
 import joblib
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="../templates")
 
-@app.get("/predict/")
-async def predict(ph: float, hardness: float, solids: float,
-                    chloramines: float, sulfate: float, conductivity: float,
-                    organic_carbon: float, trihalomethanes: float,
-                    turbidity: str):
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/predict/")
+async def predict(request: Request, ph: float = Form(...), hardness: float = Form(...),
+                solids: float = Form(...), chloramines: float = Form(...),
+                sulfate: float = Form(...), conductivity: float = Form(...),
+                organic_carbon: float = Form(...), trihalomethanes: float = Form(...),
+                turbidity: float = Form(...)):
     
     scaler = joblib.load("models/scaler.save")
     # Normalize data
@@ -17,12 +24,28 @@ async def predict(ph: float, hardness: float, solids: float,
         organic_carbon, trihalomethanes, turbidity]]
     )
 
-    # Predictive model
+    # Load Predictive model
     model = joblib.load("models/svc.save")
+    # Get predictions
     prediction = model.predict(data)
-    #probabilities = model.predict_proba(data)
+
+    positive_title = "The water is potable!"
+    positive_img_url = "https://www.snf.co.uk/wp-content/uploads/2017/06/rsz_potabledrinkingwater.jpg"
+
+    negative_title = "The water is not potable, careful!"
+    negative_img_url = "https://cdn-reichelt.de/bilder/web/xxl_ws/C180/W-74105.png"
+
+    result = "POTABLE" if prediction[0] == 1 else "NOT POTABLE"
+    img_url = positive_img_url if prediction[0] == 1 else negative_img_url
+    alert_type = "alert-primary" if prediction[0] == 1 else "alert-danger"
+    title = positive_title if prediction[0] == 1 else negative_title
+
+
     
-    return {
-        "prediction": int(prediction[0])
-        #"probabilities": probabilities
-    }
+    return templates.TemplateResponse("prediction.html", {
+        "request": request,
+        "alert_type": alert_type,
+        "result": result,
+        "img_url": img_url,
+        "title": title
+        })
